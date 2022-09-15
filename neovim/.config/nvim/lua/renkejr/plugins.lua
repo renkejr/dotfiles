694dd201c2885789bcafcd4e923a1cfcd0c2dffa
Rodney Enke <renkejr@gmail.com>
2022-09-15 17:47:37 -0500

    Added nvim-tree.
    Added nvim-tree file explorer.

--- neovim/.config/nvim/lua/renkejr/plugins.lua
+++ neovim/.config/nvim/lua/renkejr/plugins.lua
@@ -1,11 +1,16 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.lua
 
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
-  use { "catppuccin/nvim", as = "catppuccin" }
+    use 'wbthomason/packer.nvim'
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+            'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        }
+    }
+    use 'folke/tokyonight.nvim'
+    use { "catppuccin/nvim", as = "catppuccin" }
 end)
+
