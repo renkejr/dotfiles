8921efde7a3217019a57a3e5907a94b1b32f6ad3
Rodney Enke <renkejr@gmail.com>
2022-09-14 19:44:39 -0500

    Initial commit cleanup.

--- neovim/.config/nvim/lua/renkejr/set.lua
+++ neovim/.config/nvim/lua/renkejr/set.lua
@@ -1,17 +1,9 @@
--------------------------------------------------
--- Modified from DT'S NEOVIM CONFIGURATION
--- Neovim website: https://neovim.io/
--- DT's dotfiles: https://gitlab.com/dwt1/dotfiles
--------------------------------------------------
-
 local g = vim.g
 local o = vim.o
 local opt = vim.opt
-local A = vim.api
 
 -- Terminal options.
 o.termguicolors = true
--- o.background = 'dark'
 
 -- Line numbers.
 o.number = true
@@ -21,21 +13,21 @@ o.relativenumber = true
 o.timeoutlen = 500
 o.updatetime = 200
 
--- Number of screen lines to keep above and below the cursor
+-- Number of screen lines to keep.
 o.scrolloff = 5
 
--- Tabs, spacing, and indents.
+-- Tabs, spacing, wrapping, and indents.
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
+opt.wrap = false
 
 -- Search options.
 opt.hlsearch = false
 opt.incsearch = true
 
-opt.wrap = false
-
+-- Set the leader key used in the remap.lua file.
 g.mapleader = " "
 
